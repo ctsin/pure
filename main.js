be9ca7265fc2cd4a953869eea02803cc5675ad5f
邢贵志 <tsing@outlook.com>
2019-03-24 17:12:31 +0800

    Init

---
+++ main.js
@@ -0,0 +1,353 @@
+const MOCK = [
+  {
+    _id: "5c9577cb15f9f37b1473124f",
+    index: 4,
+    guid: "a60dc650-0aa7-4321-a163-a21681514240",
+    isActive: false,
+    balance: "$2,401.96",
+    picture: "http://placehold.it/32x32",
+    age: 37,
+    eyeColor: "brown",
+    name: {
+      first: "Rosalind",
+      last: "Terry"
+    },
+    company: "ZOSIS",
+    email: "rosalind.terry@zosis.me",
+    phone: "+1 (862) 553-2794",
+    address: "268 Madison Place, Brownlee, Oklahoma, 607",
+    registered: "Monday, April 11, 2016 9:08 PM",
+    tags: ["nisi", "dolore", "enim", "laborum", "id"],
+    range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    friends: [
+      {
+        id: 0,
+        name: "Corrine Grant"
+      },
+      {
+        id: 1,
+        name: "Hannah Harmon"
+      },
+      {
+        id: 2,
+        name: "Noemi Sims"
+      }
+    ],
+    greeting: "Hello, Rosalind! You have 8 unread messages.",
+    type: "SELECT"
+  },
+  {
+    _id: "5c9577cbc3559c59de6a9d0a",
+    index: 5,
+    guid: "a3539000-5f65-4e98-921e-28c7edc3093e",
+    isActive: false,
+    balance: "$1,347.50",
+    picture: "http://placehold.it/32x32",
+    age: 23,
+    eyeColor: "brown",
+    name: {
+      first: "Miles",
+      last: "Larson"
+    },
+    company: "MEDICROIX",
+    email: "miles.larson@medicroix.net",
+    phone: "+1 (840) 480-3455",
+    address: "796 Woods Place, Waterford, Utah, 7538",
+    registered: "Tuesday, September 15, 2015 7:04 PM",
+    tags: ["commodo", "ullamco", "amet", "officia", "tempor"],
+    range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    friends: [
+      {
+        id: 0,
+        name: "Hurley Levine"
+      },
+      {
+        id: 1,
+        name: "Joanne Heath"
+      },
+      {
+        id: 2,
+        name: "Roth Macdonald"
+      }
+    ],
+    greeting: "Hello, Miles! You have 9 unread messages.",
+    type: "INPUT"
+  },
+  {
+    _id: "5c9577cbb9619e6cdd4a1aa0",
+    index: 6,
+    guid: "1266a171-0f0c-4e30-a6dc-7c22203eaa92",
+    isActive: false,
+    balance: "$3,295.79",
+    picture: "http://placehold.it/32x32",
+    age: 36,
+    eyeColor: "blue",
+    name: {
+      first: "Madeleine",
+      last: "Hammond"
+    },
+    company: "KONNECT",
+    email: "madeleine.hammond@konnect.us",
+    phone: "+1 (900) 474-2914",
+    address: "597 Bulwer Place, Cressey, Kentucky, 9773",
+    registered: "Thursday, September 18, 2014 6:49 PM",
+    tags: ["reprehenderit", "sit", "deserunt", "fugiat", "et"],
+    range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    friends: [
+      {
+        id: 0,
+        name: "Neal Hodges"
+      },
+      {
+        id: 1,
+        name: "Brigitte Garrett"
+      },
+      {
+        id: 2,
+        name: "Frost Donaldson"
+      }
+    ],
+    greeting: "Hello, Madeleine! You have 5 unread messages.",
+    type: "INPUT"
+  },
+  {
+    _id: "5c9577cb425998be406be747",
+    index: 7,
+    guid: "957904ef-5767-4809-9b88-6c7641626968",
+    isActive: true,
+    balance: "$2,305.60",
+    picture: "http://placehold.it/32x32",
+    age: 31,
+    eyeColor: "blue",
+    name: {
+      first: "Chang",
+      last: "Middleton"
+    },
+    company: "QUADEEBO",
+    email: "chang.middleton@quadeebo.info",
+    phone: "+1 (970) 534-2038",
+    address: "933 Halleck Street, Zeba, Tennessee, 8995",
+    registered: "Wednesday, November 5, 2014 10:59 AM",
+    tags: ["fugiat", "occaecat", "cupidatat", "est", "ad"],
+    range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    friends: [
+      {
+        id: 0,
+        name: "Whitley Kidd"
+      },
+      {
+        id: 1,
+        name: "Aida Chavez"
+      },
+      {
+        id: 2,
+        name: "Rosanne Keller"
+      }
+    ],
+    greeting: "Hello, Chang! You have 5 unread messages.",
+    type: "INPUT"
+  },
+  {
+    _id: "5c9577cb2cd2bbfd664568ce",
+    index: 8,
+    guid: "c3fd90f1-1cfb-43db-919d-f8655c7ffa93",
+    isActive: true,
+    balance: "$2,804.66",
+    picture: "http://placehold.it/32x32",
+    age: 33,
+    eyeColor: "green",
+    name: {
+      first: "Schneider",
+      last: "Shields"
+    },
+    company: "NAMEGEN",
+    email: "schneider.shields@namegen.biz",
+    phone: "+1 (840) 533-3873",
+    address: "257 Porter Avenue, Welda, Pennsylvania, 9858",
+    registered: "Monday, December 3, 2018 2:55 PM",
+    tags: ["occaecat", "quis", "est", "dolor", "officia"],
+    range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    friends: [
+      {
+        id: 0,
+        name: "Lucinda Mcpherson"
+      },
+      {
+        id: 1,
+        name: "Johanna Compton"
+      }
+    ],
+    greeting: "Hello, Schneider! You have 6 unread messages.",
+    type: "CHECKBOX"
+  },
+  {
+    _id: "5c9577cbe5d7a363de4e17a2",
+    index: 9,
+    guid: "b92fd3d3-028d-4151-8c8d-68092848ebb6",
+    isActive: false,
+    balance: "$2,261.08",
+    picture: "http://placehold.it/32x32",
+    age: 40,
+    eyeColor: "brown",
+    name: {
+      first: "Deleon",
+      last: "Hansen"
+    },
+    company: "CUBIX",
+    email: "deleon.hansen@cubix.co.uk",
+    phone: "+1 (930) 536-2271",
+    address: "136 Bennet Court, Sisquoc, Rhode Island, 2868",
+    registered: "Wednesday, February 19, 2014 2:14 PM",
+    tags: ["quis", "fugiat", "amet", "culpa", "pariatur"],
+    range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    friends: [
+      {
+        id: 0,
+        name: "Christi Becker"
+      },
+      {
+        id: 1,
+        name: "Imogene Castaneda"
+      },
+      {
+        id: 2,
+        name: "Dena Snider"
+      }
+    ],
+    greeting: "Hello, Deleon! You have 7 unread messages.",
+    type: "PASSWORD"
+  }
+];
+
+const Z = () => {
+  /**
+   * 渲染 <label /> 视图
+   * @access private
+   */
+  const label = (label, htmlFor) => {
+    return /*html*/ `
+      <label class="label" for="${htmlFor}">${label}</label>
+    `;
+  };
+
+  /**
+   * 验证表单方法
+   * @access private
+   * @param {HTMLElement} target - 触发验证的表单控件
+   * @param {boolean} condition - true: 表单验证通过；false: 表单验证失败；
+   * @param {string} message - 验证失败时的错误提示
+   */
+  const validate = (
+    target,
+    condition,
+    message = "靓仔，你这个不能空着啊！"
+  ) => {
+    const invalid = target.parentNode.parentNode.querySelector(".invalid");
+
+    if (condition) {
+      invalid.classList.remove("in");
+      invalid.innerHTML = "";
+    } else {
+      invalid.classList.add("in");
+      invalid.innerHTML = message;
+    }
+  };
+
+  const SELECT = ({ name: { first, last }, guid, tags }) => {
+    $(document).on("change", `#${guid}`, ({ target }) => {
+      validate(target, target.value.trim(), "不中啊！怎么说也得选一个 -_-!");
+    });
+
+    return /*html*/ `
+      <div class="field">
+        ${(first || last) && label(`${first} ${last}`, guid)}
+        <div class="control">
+          <select class="select" name="${guid}" id="${guid}">
+            <option value="">请选择</option>
+            ${tags.map(tag => {
+              return /*html*/ `
+                <option value="${tag}">${tag}</option>
+              `;
+            })}
+          </select>
+          <div class="suffix"></div>
+        </div>
+        <div class="invalid"></div>
+      </div>
+    `;
+  };
+
+  const INPUT = ({ name: { first, last }, guid }) => {
+    $(document).on("focusout", `#${guid}`, ({ target }) => {
+      validate(target, target.value.trim());
+    });
+
+    return /*html*/ `
+      <div class="field">
+        ${(first || last) && label(`${first} ${last}`, guid)}
+        <div class="control">
+          <input class="input" id="${guid}" name="${guid}" type="text" />
+          <div class="suffix"></div>
+        </div>
+        <div class="invalid"></div>
+      </div>
+    `;
+  };
+
+  const PASSWORD = ({ name: { first, last }, guid }) => {
+    return /*html*/ `
+      <div class="field">
+        ${(first || last) && label(`${first} ${last}`, guid)}
+        <div class="control">
+          <input class="input" id="${guid}" name="${guid}" type="password" />
+          <div class="suffix">必要</div>
+        </div>
+        <div class="invalid"></div>
+      </div>
+    `;
+  };
+
+  const CHECKBOX = ({ friends }) => {
+    const units = ({ id, name }) => /*html*/ `
+      <input id="${id}" name="${id}" type="checkbox" />
+      ${name && label(`${name}`, id)}
+    `;
+
+    return /*html*/ `
+      <div class="field">
+        ${
+          Array.isArray(friends)
+            ? friends
+                .map(friend => {
+                  return /*html*/ `
+                  <div class="col-${friends.length}">${units(friend)}</div>
+                `;
+                })
+                .join("")
+            : units(friends)
+        }
+        <div class="invalid"></div>
+      </div>
+    `;
+  };
+
+  return { SELECT, INPUT, PASSWORD, CHECKBOX };
+};
+
+const render = () => {
+  const inputs = Z();
+
+  return /*html*/ `
+    <form class="form ">
+      ${MOCK.map(data => {
+        return /*html*/ `
+          ${inputs[data.type](data)}
+        `;
+      }).join("")}
+
+      <button class="button" type="submit">提交</button>
+    </form>
+  `;
+};
+
+document.body.innerHTML = render();
